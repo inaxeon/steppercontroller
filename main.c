34dc937daf3131f5e5f06711618c3cb9ebf0c38c
Matt <Matt@D1NC01>
2023-04-08 06:09:36 +0100

    Up/down buttons

--- main.c
+++ main.c
@@ -38,12 +38,16 @@
 #include "stepper.h"
 
 #define F_CHANGE_DIR        0x01
+#define F_UP                0x02
+#define F_DOWN              0x04
 
 typedef struct
 {
     sys_config_t *config;
     uint8_t runflags;
+    uint8_t last_portb;
     uint8_t last_portc;
+    uint8_t last_portd;
 } sys_runstate_t;
 
 sys_config_t _g_cfg;
@@ -52,9 +56,25 @@ sys_runstate_t _g_rs;
 FILE uart_str = FDEV_SETUP_STREAM(print_char, NULL, _FDEV_SETUP_RW);
 
 static void io_init(void);
+static void port_change_interrupt(void);
 //static void test_1s(void *param);
 
+ISR(PCINT0_vect)
+{
+    port_change_interrupt();
+}
+
 ISR(PCINT1_vect)
+{
+    port_change_interrupt();
+}
+
+ISR(PCINT2_vect)
+{
+    port_change_interrupt();
+}
+
+static void port_change_interrupt(void)
 {
     if (!(_g_rs.last_portc & _BV(PC1)) && (PINC & _BV(PC1)))
     {
@@ -62,7 +82,21 @@ ISR(PCINT1_vect)
         PCMSK1 &= ~_BV(PCINT9); // Disable interrupt
     }
 
+    if (!(_g_rs.last_portb & _BV(PB0)) && (PINB & _BV(PB0)))
+    {
+        _g_rs.runflags |= F_UP;
+        PCMSK0 &= ~_BV(PCINT0); // Disable interrupt
+    }
+
+    if (!(_g_rs.last_portd & _BV(PD5)) && (PIND & _BV(PD5)))
+    {
+        _g_rs.runflags |= F_DOWN;
+        PCMSK2 &= ~_BV(PCINT21); // Disable interrupt
+    }
+
+    _g_rs.last_portb = PINB;
     _g_rs.last_portc = PINC;
+    _g_rs.last_portd = PIND;
 }
 
 int main(void)
@@ -114,6 +148,22 @@ int main(void)
             PCIFR |= _BV(PCIE1);
             PCMSK1 |= _BV(PCINT9);
         }
+
+        if (rs->runflags & F_UP)
+        {
+            rs->runflags &= ~F_UP;
+            stepper_move_fixed_count(STEP_REVERSE, 3250);
+            PCIFR |= _BV(PCIE0);
+            PCMSK0 |= _BV(PCINT0);
+        }
+
+        if (rs->runflags & F_DOWN)
+        {
+            rs->runflags &= ~F_DOWN;
+            stepper_move_fixed_count(STEP_FORWARD, 3250);
+            PCIFR |= _BV(PCIE2);
+            PCMSK2 |= _BV(PCINT21);
+        }
     }
 }
 
@@ -126,8 +176,18 @@ static void io_init(void)
 #endif
 
     _g_rs.last_portc = PINC;
+    _g_rs.last_portb = PINB;
+    _g_rs.last_portd = PIND;
 
+    PCICR |= _BV(PCIE0);
     PCICR |= _BV(PCIE1);
+    PCICR |= _BV(PCIE2);
+
+    // Up button
+    PCMSK0 |= _BV(PCINT0);
+    // Down button
+    PCMSK2 |= _BV(PCINT21);
+    // Reverse
     PCMSK1 |= _BV(PCINT9);
 }
 
